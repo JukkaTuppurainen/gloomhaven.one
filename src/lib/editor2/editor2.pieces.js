1c81338f49165f201833a49d1ba733e8c3120e8c
Jukka Tuppurainen <jukka@tuppurainen.net>
2019-05-17 16:23:43 +0300

    Add the rest of pieces

--- src/lib/editor2/editor2.pieces.js
+++ src/lib/editor2/editor2.pieces.js
@@ -1,5 +1,4 @@
 import bitmap_door from '../../scenarios/door.webp'
-
 import bitmap_A1a  from '../../scenarios/A1a.webp'
 import bitmap_A1b  from '../../scenarios/A1b.webp'
 import bitmap_A2a  from '../../scenarios/A2a.webp'
@@ -8,7 +7,6 @@ import bitmap_A3a  from '../../scenarios/A3a.webp'
 import bitmap_A3b  from '../../scenarios/A3b.webp'
 import bitmap_A4a  from '../../scenarios/A4a.webp'
 import bitmap_A4b  from '../../scenarios/A4b.webp'
-
 import bitmap_B1a  from '../../scenarios/B1a.webp'
 import bitmap_B1b  from '../../scenarios/B1b.webp'
 import bitmap_B2a  from '../../scenarios/B2a.webp'
@@ -17,38 +15,50 @@ import bitmap_B3a  from '../../scenarios/B3a.webp'
 import bitmap_B3b  from '../../scenarios/B3b.webp'
 import bitmap_B4a  from '../../scenarios/B4a.webp'
 import bitmap_B4b  from '../../scenarios/B4b.webp'
-
 import bitmap_C1a  from '../../scenarios/C1a.webp'
 import bitmap_C1b  from '../../scenarios/C1b.webp'
 import bitmap_C2a  from '../../scenarios/C2a.webp'
 import bitmap_C2b  from '../../scenarios/C2b.webp'
-
 import bitmap_D1a  from '../../scenarios/D1a.webp'
 import bitmap_D1b  from '../../scenarios/D1b.webp'
 import bitmap_D2a  from '../../scenarios/D2a.webp'
 import bitmap_D2b  from '../../scenarios/D2b.webp'
-
 import bitmap_E1a  from '../../scenarios/E1a.webp'
 import bitmap_E1b  from '../../scenarios/E1b.webp'
-
 import bitmap_F1a  from '../../scenarios/F1a.webp'
 import bitmap_F1b  from '../../scenarios/F1b.webp'
-
 import bitmap_G1a  from '../../scenarios/G1a.webp'
 import bitmap_G1b  from '../../scenarios/G1b.webp'
 import bitmap_G2a  from '../../scenarios/G2a.webp'
 import bitmap_G2b  from '../../scenarios/G2b.webp'
-
 import bitmap_H1a  from '../../scenarios/H1a.webp'
 import bitmap_H1b  from '../../scenarios/H1b.webp'
 import bitmap_H2a  from '../../scenarios/H2a.webp'
 import bitmap_H2b  from '../../scenarios/H2b.webp'
 import bitmap_H3a  from '../../scenarios/H3a.webp'
 import bitmap_H3b  from '../../scenarios/H3b.webp'
-
+import bitmap_I1a  from '../../scenarios/I1a.webp'
 import bitmap_I1b  from '../../scenarios/I1b.webp'
+import bitmap_I2a  from '../../scenarios/I2a.webp'
+import bitmap_I2b  from '../../scenarios/I2b.webp'
 import bitmap_J1a  from '../../scenarios/J1a.webp'
+import bitmap_J1b  from '../../scenarios/J1b.webp'
+import bitmap_J2a  from '../../scenarios/J2a.webp'
+import bitmap_J2b  from '../../scenarios/J2b.webp'
+import bitmap_K1a  from '../../scenarios/K1a.webp'
+import bitmap_K1b  from '../../scenarios/K1b.webp'
+import bitmap_K2a  from '../../scenarios/K2a.webp'
+import bitmap_K2b  from '../../scenarios/K2b.webp'
 import bitmap_L1a  from '../../scenarios/L1a.webp'
+import bitmap_L1b  from '../../scenarios/L1b.webp'
+import bitmap_L2a  from '../../scenarios/L2a.webp'
+import bitmap_L2b  from '../../scenarios/L2b.webp'
+import bitmap_L3a  from '../../scenarios/L3a.webp'
+import bitmap_L3b  from '../../scenarios/L3b.webp'
+import bitmap_M1a  from '../../scenarios/M1a.webp'
+import bitmap_M1b  from '../../scenarios/M1b.webp'
+import bitmap_N1a  from '../../scenarios/N1a.webp'
+import bitmap_N1b  from '../../scenarios/N1b.webp'
 
 
 const blueprints_A = {
@@ -121,6 +131,74 @@ const styles_H = {
   180: {top: 27}
 }
 
+const blueprints_I = {
+  0: 'afaeafaeaf.1ae1f3ae3f5ae5f'
+  // 60: 'ddcecgbfbfaecedd.1dc2ec3gc6af7cf8df',
+  // 120: 'ddbdaeaebfbfcecc.1dd2bd3ad6fa7ea8ca'
+}
+
+const styles_I = {
+  0: {left: -51, top: -22},
+  // 60: {left: 76, top: 47},
+  // 120: {left: 75, top: -5},
+  180: {left: -53, top: -23}
+}
+
+const blueprints_Ja = {
+  0: 'aeafagegfhfhgg.1ae1ec2fc3ae6ha7ga',
+  180: 'ccacbdbdcichei.1cd2ad5i6cf7ef7i'
+}
+
+const styles_Ja = {
+  180: {left: -32, top: -3}
+}
+
+const blueprints_Jb = {
+  0: 'ggfhfhegagafae.1gc2hc5ae6fa7ae7ea',
+  180: 'eichcibdbdaccc.1ed1i2cd3i6af7cf'
+}
+
+const styles_Jb = {
+  0: {left: -33},
+  180: {left: -53, top: -5}
+}
+
+const blueprints_K = {
+  0: 'cfbfbgacegacfhabfg',
+  180: 'bcghacfhbdfhbgcgcf',
+}
+
+const styles_K = {
+  0: {left: -33, top: -57},
+  180: {left: -30, top: -3}
+}
+
+const blueprints_L = {
+  0: 'aeadaeadaeadae.1ae1e3ae3e5ae5e7ae7e'
+}
+
+const styles_L = {
+  0: {top: -22}
+}
+
+const blueprints_M = {
+  0: 'beaeafaeafaebe.3ae3f5ae5f'
+}
+
+const styles_M = {
+  0: {top: -5},
+  180: {left: -53, top: -2}
+}
+
+const blueprints_N = {
+  0: 'ahagahagahagah.1ae1h3ae3h5ae5h7ae7h'
+}
+
+const styles_N = {
+  0: {left: -52, top: -22},
+  180: {left: -52, top: -24}
+}
+
 export const pieceList = {
   door: {
     bitmap: bitmap_door,
@@ -132,8 +210,6 @@ export const pieceList = {
     },
     symmetrical: true
   },
-
-  // -------- A --------
 
   A1a: {
     bitmap: bitmap_A1a,
@@ -183,8 +259,6 @@ export const pieceList = {
     styles: styles_A
   },
 
-  // -------- B --------
-
   B1a: {
     bitmap: bitmap_B1a,
     blueprints: blueprints_B,
@@ -233,8 +307,6 @@ export const pieceList = {
     styles: styles_B
   },
 
-  // -------- C --------
-
   C1a: {
     bitmap: bitmap_C1a,
     blueprints: blueprints_C,
@@ -258,8 +330,6 @@ export const pieceList = {
     blueprints: blueprints_C,
     styles: styles_C
   },
-
-  // -------- D --------
 
   D1a: {
     bitmap: bitmap_D1a,
@@ -289,8 +359,6 @@ export const pieceList = {
     symmetrical: true
   },
 
-  // -------- E --------
-
   E1a: {
     bitmap: bitmap_E1a,
     blueprints: blueprints_E,
@@ -305,8 +373,6 @@ export const pieceList = {
     symmetrical: true
   },
 
-  // -------- F --------
-
   F1a: {
     bitmap: bitmap_F1a,
     blueprints: blueprints_F,
@@ -320,8 +386,6 @@ export const pieceList = {
     styles: styles_F,
     symmetrical: true
   },
-
-  // -------- G --------
 
   G1a: {
     bitmap: bitmap_G1a,
@@ -346,8 +410,6 @@ export const pieceList = {
     blueprints: blueprints_G,
     symmetrical: true
   },
-
-  // -------- H --------
 
   H1a: {
     bitmap: bitmap_H1a,
@@ -385,42 +447,152 @@ export const pieceList = {
     styles: styles_H
   },
 
-  // -------- other --------
+  I1a: {
+    bitmap: bitmap_I1a,
+    blueprints: blueprints_I,
+    styles: styles_I,
+    symmetrical: true
+  },
 
   I1b: {
     bitmap: bitmap_I1b,
-    blueprints: {
-      0: 'afaeafaeaf.1ae1f3ae3f5ae5f',
-      60: 'ddcecgbfbfaecedd.1dc2ec3gc6af7cf8df',
-      120: 'ddbdaeaebfbfcecc.1dd2bd3ad6fa7ea8ca'
-    },
+    blueprints: blueprints_I,
     styles: {
-      60: {left: 76, top: 47},
-      120: {left: 75, top: -5},
-      180: {left: -48, top: -20}
+      0: {left: -56, top: -27},
+      180: {left: -47, top: -18}
     },
+    symmetrical: true
+  },
+
+  I2a: {
+    bitmap: bitmap_I2a,
+    blueprints: blueprints_I,
+    styles: styles_I,
+    symmetrical: true
+  },
+
+  I2b: {
+    bitmap: bitmap_I2b,
+    blueprints: blueprints_I,
+    styles: styles_I,
     symmetrical: true
   },
 
   J1a: {
     bitmap: bitmap_J1a,
-    blueprints: {
-      0: 'aeafagegfhfhgg.1ae1ec2fc3ae6ha7ga',
-      180: 'ccacbdbdcichei.1cd2ad5i6cf7ef7i'
-    },
-    styles: {
-      180: {left: -32, top: -3}
-    }
+    blueprints: blueprints_Ja,
+    styles: styles_Ja
+  },
+
+  J1b: {
+    bitmap: bitmap_J1b,
+    blueprints: blueprints_Jb,
+    styles: styles_Jb
+  },
+
+  J2a: {
+    bitmap: bitmap_J2a,
+    blueprints: blueprints_Ja,
+    styles: styles_Ja
+  },
+
+  J2b: {
+    bitmap: bitmap_J2b,
+    blueprints: blueprints_Jb,
+    styles: styles_Jb
+  },
+
+  K1a: {
+    bitmap: bitmap_K1a,
+    blueprints: blueprints_K,
+    styles: styles_K
+  },
+
+  K1b: {
+    bitmap: bitmap_K1b,
+    blueprints: blueprints_K,
+    styles: styles_K
+  },
+
+  K2a: {
+    bitmap: bitmap_K2a,
+    blueprints: blueprints_K,
+    styles: styles_K
+  },
+
+  K2b: {
+    bitmap: bitmap_K2b,
+    blueprints: blueprints_K,
+    styles: styles_K
   },
 
   L1a: {
     bitmap: bitmap_L1a,
-    blueprints: {
-      0: 'aeadaeadaeadae.1ae1e3ae3e5ae5e7ae7e'
-    },
-    styles: {
-      180: {left: -51, top: -23}
-    },
+    blueprints: blueprints_L,
+    styles: styles_L,
+    symmetrical: true
+  },
+
+  L1b: {
+    bitmap: bitmap_L1b,
+    blueprints: blueprints_L,
+    styles: styles_L,
+    symmetrical: true
+  },
+
+  L2a: {
+    bitmap: bitmap_L2a,
+    blueprints: blueprints_L,
+    styles: styles_L,
+    symmetrical: true
+  },
+
+  L2b: {
+    bitmap: bitmap_L2b,
+    blueprints: blueprints_L,
+    styles: styles_L,
+    symmetrical: true
+  },
+
+  L3a: {
+    bitmap: bitmap_L3a,
+    blueprints: blueprints_L,
+    styles: styles_L,
+    symmetrical: true
+  },
+
+  L3b: {
+    bitmap: bitmap_L3b,
+    blueprints: blueprints_L,
+    styles: styles_L,
+    symmetrical: true
+  },
+
+  M1a: {
+    bitmap: bitmap_M1a,
+    blueprints: blueprints_M,
+    styles: styles_M,
+    symmetrical: true
+  },
+
+  M1b: {
+    bitmap: bitmap_M1b,
+    blueprints: blueprints_M,
+    styles: styles_M,
+    symmetrical: true
+  },
+
+  N1a: {
+    bitmap: bitmap_N1a,
+    blueprints: blueprints_N,
+    styles: styles_N,
+    symmetrical: true
+  },
+
+  N1b: {
+    bitmap: bitmap_N1b,
+    blueprints: blueprints_N,
+    styles: styles_N,
     symmetrical: true
   }
 }
