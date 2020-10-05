2937c555aeba311e94e75c6b628184cfd9c7b683
Charles Sarrazin-Boucher <49283485+superchuck8@users.noreply.github.com>
2020-10-05 15:09:53 -0400

    Scenarios/up to95 (#7)
    Complete scenarios up to 95
    
    Co-authored-by: Charles Sarrazin Boucher <CBoucher@CegepGranby.qc.ca>
    Co-authored-by: csboucher <csboucher@kinesiq.com>

--- src/scenarios.js
+++ src/scenarios.js
@@ -159,6 +159,50 @@ export const scenarioList = {
     'Ancient Defense Network',
     '12mA4bc3bC1a15lD1a18hD2ae1eK2be8aL1ac10gM1ac9i017i014n019l03e07c0'
   ],
+  41: [
+    'Timeworn Tomb',
+    '9bE1ac8hL1a8mM1a11l011g0'
+  ],
+  42: [
+    'Realm of the Voice',
+    '12eJ1b4eJ2bb8cL2ac15e67e6'
+  ],
+  43: [
+    'Drake Nest',
+    '17jA2b17dA3a10aE1bc5fG2ac10nI2ac9fN1a12n212f28g28m216l216h2'
+  ],
+  44: [
+    'Tribal Assault',
+    '8mB1bb14dB3ae8aB4ae4dL1be10gM1bb11m7b10i310m39d115g1'
+  ],
+  45: [
+    'Rebel Swamp',
+    '12oD1bb2bF1ba13eG1aa7hM1bd12o313j38i3'
+  ],
+  46: [
+    'Nightmare Peak',
+    '8mI2ae4bJ1bc8gK2ab7i213m2'
+  ],
+  47: [
+    'Lair of the Unseeing Eye',
+    '3bJ1ab13cM1ae13e0'
+  ],
+  48: [
+    'Shadow Weald',
+    '8fL1b8aL3ac8d7a8e7a10e711d7a14e713e3'
+  ],
+  49: [
+    'Rebel\'s Stand',
+    '10aG1ac8nL1b8iL3ac8m710m712m714m711h39m311m313m3'
+  ],
+  50: [
+    'Ghost Fortress',
+    '10mB2bc10aB3bc17iC1ac17dC2bc3fI1b9eN1b16f416k412e012m08i0'
+  ],
+  51: [
+    'The Void',
+    '7aD1ac4fD2ab10fM1a9g410e4a7e4b9f0'
+  ],
   52: [
     'Noxious Cellar',
     '2kJ1a10dJ2ac12nK1ae1aK2b6iM1a6i013j05l012n06lx7kx8kx9ix9jx9kx9lx9mx10lx11kx12kx'
@@ -170,5 +214,169 @@ export const scenarioList = {
   54: [
     'Palace of Ice',
     '4aG2a2iN1a5h2'
+  ],
+  55: [
+    'Foggy Thicket',
+    ''
+  ],
+  56: [
+    'Bandit\'s Wood',
+    '11eA4ac6hC2a1aL3aa14dM1bc7g310i313f3'
+  ],
+  57: [
+    'Investigation',
+    '16bA1a4gC1ac19gC2b9gF1bc9aI1bc8i010g016g018i3'
+  ],
+  58: [
+    'Bloody Shack',
+    '7dB1ac16hC2a16aD1bb12cG2b15c115i111f1'
+  ],
+  59: [
+    'Forgotten Grove',
+    '16fA4ac10gD1ba14aL3a2gM1bc15h717e39h3'
+  ],
+  60: [
+    'Alchemy Lab',
+    '5aB1ac16fB2ac7hB3ac16lB4ac12gG2b10aI2bc12f514f59c115g113f111j115m1'
+  ],
+  61: [
+    'Fading Lighthouse',
+    '8jA1ae12cA3bb16aC1a8eD1aa5lM1ab10m09i012e016d0'
+  ],
+  62: [
+    'Pit of Souls',
+    '11aB2be7dM1ab11d4b13e0'
+  ],
+  63: [
+    'Magma Pit',
+    '14bD2b8aI2ac1bK1b7c213c2'
+  ],
+  64: [
+    'Underwater Lagoon',
+    '2aD2be6eK1be12aM1bb6e213g2'
+  ],
+  65: [
+    'Sulfur Mine',
+    '15lB1a15aB2a7lB3a7aB4a16eH1a8eH2a2fI2bc15h27h28e216e28l216l2'
+  ],
+  66: [
+    'Clockwork Cove',
+    '18hA1a12qA2aa10eC1ac4nC2bb12iD1ad9mD2ae2aJ1be9f013h017j013m08p013q0'
+  ],
+  67: [
+    'Arcane Library',
+    '14fA2ac10cG1b14aL3ac12kM1ac13h413f415j013c3'
+  ],
+  68: [
+    'Toxic Moor',
+    '14jB1b18aC2ac14bG1a6iM1b17b716i714i715i313k3'
+  ],
+  69: [
+    'Well of the Unfortunate',
+    '14kB1b14dD2ad8aJ1aa6jL3ac9i014d013l3'
+  ],
+  70: [
+    'Chained Isle',
+    '11gA2bd9kA3a12kD2bd9bE1bd1mL1b1hL3a1l73l75l77l714g213j211m26m34m32m38m2'
+  ],
+  71: [
+    'Wingswept Highlands',
+    '12qA2b17cA3a9lA4ac11aB1b4fC2ab20jG2a12jH2b9eL3a8g711d713d713i711m712e314e316g212j312q219m2'
+  ],
+  72: [
+    'Oozing Grove',
+    '8lL1bc8aL3a8fM1bc10e712e712k710k711e311k3'
+  ],
+  73: [
+    'Rockslide Ridge',
+    '9kE1b8aL3a8fM1bc10e712e711e311k2'
+  ],
+  74: [
+    'Merchant Ship',
+    '2eB1ac7cG2b11gI1ac11aI2bc10f110i16g1'
+  ],
+  75: [
+    'Overgrown Graveyard',
+    '2kB1bc3aG1a7eL1bc7jL3ac7i79i711i713i76g36m312j310j38j3'
+  ],
+  76: [
+    'Harrower Hive',
+    '7kA2bc4kA3ac10aB1bc10lB4bc6eE1b15jG2a12eH2a3c616d620l69q66q613q6'
+  ],
+  77: [
+    'Vault of Secrets',
+    '11mB2bc11aB3bc2gM1a10fN1b9i013e013m0'
+  ],
+  78: [
+    'Sacrifice Pit',
+    '2gF1ac14aI1b6aM1a13c49f013b0'
+  ],
+  79: [
+    'Lost Temple',
+    '6aC2ba11kD2a5oK1ab17iK2b7dM1aa8d4a16m411o4b12k0'
+  ],
+  80: [
+    'Vigil Keep',
+    '1cA1ac2mB1ae6iD1bc4aH1b10hJ1ad12cL1a3d011d015g010m36m1'
+  ],
+  81: [
+    'Temple of the Eclipse',
+    '13jD1ab8jJ1a8aJ2aa14o09i0'
+  ],
+  82: [
+    'Burning Mountain',
+    '15fB2bc9bC1a9gD1ae14aI1bc2cK1a11e417e4a8d08i011g014i016g015f013c0'
+  ],
+  83: [
+    'Shadows Within',
+    '1bH1b9cI1bc15cM1ac14f08f0'
+  ],
+  84: [
+    'Crystalline Cave',
+    '15fA2b4gA3ac8aE1bc7fL2a10f214h214i26h26i2'
+  ],
+  85: [
+    'Sun Temple',
+    '1hC1ac2cC2bc16hD1ac13cD2ae6jF1a7bI1b16g4a6e015j012e05j0'
+  ],
+  86: [
+    'Harried Village',
+    '9bA4ac12aB1bc2hB2ac15hB3a10mH3a7gM1b11b711l56j114j110m1'
+  ],
+  87: [
+    'Corrupted Cove',
+    '4gD2bd10fL2ac10aL3bc16e614e612e610e69i211e213e215e2'
+  ],
+  88: [
+    'Plane of Water',
+    '15aD2bb7bG2ae3eN1ab15e27e29f211g213h2'
+  ],
+  89: [
+    'Syndicate Hideout',
+    '10gB1a6aB2ac13mB3a2eH3ac7lI2b8e19h18l112o1'
+  ],
+  90: [
+    'Demonic Rift',
+    '15cC2aa9cD1bd1bM1b'
+  ],
+  91: [
+    'Wild Melee',
+    '9cG1a1cH2b13aM1b12d38g3'
+  ],
+  92: [
+    'Back Alley Brawl',
+    '3aF1a10dH3b10d0'
+  ],
+  93: [
+    'Sunken Vesse',
+    '18dB3ad4hG1ae11dI1ad6aK2aa13e26h218g1'
+  ],
+  94: [
+    'Vermling Nest',
+    '4eC2aa1iD1bd10aH2b7hM1b10h37h34i36k3'
+  ],
+  95: [
+    'Payment Due',
+    '3jD1ad8aE1ac9fG1bc13gK2bb10f012j08l0'
   ]
 }
